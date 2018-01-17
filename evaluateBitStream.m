0182284ec3f2ed3c27a75353b3f46475825bbb19
Malte Hinrichs <malte.hinrichs@hhi-extern.fraunhofer.de>
2018-01-17 15:09:10 +0100

    Added Runs Test.

--- evaluateBitStream.m
+++ evaluateBitStream.m
@@ -1,28 +1,52 @@
-function [ stats ] = evaluateBitStream( bitStream, opt )
-% Statistical testing of a pseudorandom bit stream according to
+% Statistical testing of a pseudorandom bit streams according to
 % NIST Statistical Test Suite
 % https://csrc.nist.gov/Projects/Random-Bit-Generation/Documentation-and-Software
+%
+% Inputs:
+%   bitStream:  n x m Matrix. Bit stream(s) under evaluation in columns.
+%   opt:        Parameter structure
+%
+% Output:
+%   stats:      structure containing the results returned by every test in
+%               sub-structures
+function [stats] = evaluateBitStream(bitStream, opt)
 
 stats = struct;
+if ~isfield(opt,'n')
+    opt.n = size(bitStream,1);
+end
 
-if opt.frequencyTest.active
+% Frequency Test
+if isfield(opt,'freq') && opt.freq.active
     stats.frequency = frequencyTest(bitStream, opt.n);
     
     % If frequency test fails, other tests are likely to fail, too
     failedIdx = find(stats.frequency.p_value < 0.01);
     if ~isempty(failedIdx)
-        s = input(sprintf('Frequency test resulted in non-randomness of %d sequence(s). Continue? (y/N) ', length(failedIdx)),'s');
+        s = input(sprintf(['Frequency test resulted in non-randomness of ' ...
+                           '%d sequence(s). Continue? (y/N) '], length(failedIdx)),'s');
         if ~strcmpi(s,'y')
             return
         end
     end
-    [p_val_min, p_idx_min] = min(stats.frequency.p_value);
-    fprintf('Frequency test: min(p_value) = %d, sequence %d\n', p_val_min, p_idx_min);
 end
-if opt.blockFrequencyTest.active
+
+% Block Frequency Test
+if isfield(opt,'blockFreq') && opt.blockFreq.active
+    if ~isfield(opt,'M')
+        error('Block frequency test not possible: M is not defined');
+    end
     stats.blockFrequency = blockFrequencyTest(bitStream, opt.M, opt.n);
-    [p_val_min, p_idx_min] = min(stats.blockFrequency.p_value);
-    fprintf('Block frequency test: min(p_value) = %d, sequence %d\n', p_val_min, p_idx_min);
+end
+
+% Cumulative Sums Test
+if isfield(opt,'cumSums') && opt.cumSums.active
+    stats.cumulativeSums = cumulativeSumsTest(bitStream, opt.n);
+end
+
+% Runs Test
+if isfield(opt,'runs') && opt.runs.active
+    stats.runs = runsTest(bitStream, opt.n);
 end
 
 end
---
+++ runsTest.m
@@ -0,0 +1,26 @@
+function [results] = runsTest(bitStream, n)
+
+if nargin < 2
+    n = size(bitStream,1);
+end
+
+% Truncate and make 0/1
+bitStream = bitStream(1:n,:);
+if ~isempty(find(bitStream < 0, 1))
+    bitStream = (bitStream > 0);
+end
+
+% Proportion of ones (pi)
+pi_r = sum(bitStream,1) / n;
+estCrit = abs(pi_r - 0.5) > 2/sqrt(n);
+% Zero all streams for which Pi condition is not fulfilled
+bitStream(:,estCrit) = 0;
+
+% Compute V_n
+results.V = sum(xor(bitStream(1:end-1,:), bitStream(2:end,:)),1) + 1;
+
+% Compute P-value
+results.p_value = erfc(abs(results.V - 2*n * pi_r .* (1-pi_r)) ...
+                       ./ (2*sqrt(2*n) * pi_r .* (1-pi_r))); 
+end
+
